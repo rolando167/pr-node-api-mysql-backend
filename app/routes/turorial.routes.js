cae65fe6cffca88959f14694535c8a37ebc378a6
Rolando HR <rolando167@hotmail.com>
2021-10-23 12:41:30 -0500

    Backend

---
+++ app/routes/turorial.routes.js
@@ -0,0 +1,41 @@
+/*
+Define Routes
+
+When a client sends request for an endpoint using HTTP request (GET, POST, PUT, DELETE), we need to determine how the server will reponse by setting up the routes.
+
+These are our routes:
+
+    /api/tutorials: GET, POST, DELETE
+    /api/tutorials/:id: GET, PUT, DELETE
+    /api/tutorials/published: GET
+
+Create a turorial.routes.js inside app/routes folder with content like this:
+*/
+module.exports = app => {
+    const tutorials = require("../controllers/tutorial.controller.js");
+
+    var router = require("express").Router();
+
+    // Create a new Tutorial
+    router.post("/", tutorials.create);
+
+    // Retrieve all Tutorials
+    router.get("/", tutorials.findAll);
+
+    // Retrieve all published Tutorials
+    router.get("/published", tutorials.findAllPublished);
+
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
+
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
+
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+
+    // Delete all Tutorials
+    router.delete("/", tutorials.deleteAll);
+
+    app.use('/api/tutorials', router);
+};
